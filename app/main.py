bb4647f8e6dcd7e164c5f94a8d4bb064d1d8420b
Riyan Malik <riyan@example.com>
2026-03-02 14:42:22 +0530

    Changes Committed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+# Read greeting from environment variable
+greeting = os.environ.get("GREETING", "Hello World!")
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return greeting
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
